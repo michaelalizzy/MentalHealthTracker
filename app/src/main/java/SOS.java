bd35b8e0136a7fea0919adefc86bb794eb296cd1
J L <J L>
2017-02-25 22:33:31 -0500

    Courtney saves the day

--- app/src/main/java/SOS.java
+++ app/src/main/java/SOS.java
@@ -22,15 +22,15 @@ public class SOS extends AppCompatActivity {
         setContentView(R.layout.activity_sos);
 
         if (ContextCompact.checkSelfPermission(MainActivity.this,
-                Manifest.Permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                Manifest.Permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
                 if (ActivityCompat.ShouldShowrequestPermissionRationale(MainActivity.this,
-                        Manifest.permission.READ_PHONE_STATE){
+                        Manifest.permission.SEND_SMS){
 
                     ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[] {Manifest.permission.READ_PHONE_STATE}, 1);
+                            new String[] {Manifest.permission.SEND_SMS}, 1);
             }else {
                 ActivityCompat.requestPermissions(MainActivity.this,
-                        new String[] {Manifest.permission.READ_PHONE_STATE}, 1);
+                        new String[] {Manifest.permission.SEND_SMS}, 1);
             }
 
         }else {
@@ -38,14 +38,33 @@ public class SOS extends AppCompatActivity {
         }
     }
 
+    button = (Button) findViewById(R.id.button);
 
+   Text1(import from user)
+      String Text2 =  "SOS please call me";
+
+    button.setOnClickListener(new View.OnClickListener() {
+        @override
+        public void onClick(View v) {
+            String number = Text1.getText().toString();
+            String sms = Text2;
+
+            try {
+                smsManager=smsManager.getDefault();
+                smsManager.sendTextMessage (number, null, sms, null, null);
+                Toast.makeText(MainActivity.this, "Sent", Toast.LENGTH_SHORT).show();
+            }catch (exception e) {
+                Toast.makeText(MainActivity.this, "not sent", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @overridde
     public void onRequestPermissionsResult(int RequestCode, String[] permissions, int[] grantResults){
         switch (requestCode) {
             if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 if(ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.READ_PHONE_STATE)== PackageManager.PERMISSION_GRANTED){
+                        Manifest.permission.SEND_SMS)== PackageManager.PERMISSION_GRANTED){
                     Toadt.makeText(this, "Permission granted"), Toast.LENGTH_SHORT).show();
                 }
             }else{
